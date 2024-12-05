35cd8eff446615054e9ac61b6abb29e93f24fdeb
xlibnui_makix <103821356+ZastavnyeMishaUwU@users.noreply.github.com>
2024-12-05 22:26:25 +0200

    Add project files.

---
+++ dz22_11-2/Program.cs
@@ -0,0 +1,63 @@
+﻿namespace dz22_11_2
+{
+    abstract class Worker
+    {
+        public abstract void Print();
+    }
+
+    class President : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Президент: Керує всією компанією");
+        }
+    }
+
+    class Security : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Охоронець: Відповідає за безпеку працівників");
+        }
+    }
+
+    class Manager : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Менеджер: Керує проектами");
+        }
+    }
+
+    class Engineer : Worker
+    {
+        public override void Print()
+        {
+            Console.WriteLine("Інженер: Розробляє");
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            static void Main(string[] args)
+            {
+               
+                Worker[] workers = new Worker[]
+                {
+            new President(),
+            new Security(),
+            new Manager(),
+            new Engineer()
+                };
+
+                
+                foreach (Worker worker in workers)
+                {
+                    worker.Print();
+                }
+            }
+        }
+    }
+}
